a2dd3d32f28423127c0d04a3dd23fe0b0dd9c346
Carol <carol.tsai0224@gmail.com>
2022-05-05 14:28:05 -0400

    started user controllers

---
+++ controllers/userController.js
@@ -0,0 +1,37 @@
+const {User, Thought} = require('../models');
+
+module.exports = {
+   getUsers(req, res) {
+      User.find()
+         .then((users) => res.json(users))
+         .catch((err) => res.status(500).json(err));
+   },
+   getSingleUser(req, res) {
+      User.findOne({ _id: req.params.userId })
+         .select('-__v')
+         .then((user) =>
+            !user
+               ? res.status(404).json({ message: 'No user with that ID' })
+               : res.json(user)
+         )
+         .catch((err) => res.status(500).json(err));
+   },
+   createUser(req, res) {
+      User.create(req.body)
+         .then((dbUserData) => res.json(dbUserData))
+         .catch((err) => res.status(500).json(err));
+   },
+
+   deleteUser(req, res) {
+      Student.findOneAndRemove({ _id: req.params.userId })
+         .then((user) =>
+            !user
+               ? res.status(404).json({ message: 'No such user exists' })
+               : res.json(user)
+         )
+         .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+         });
+   },
+}
