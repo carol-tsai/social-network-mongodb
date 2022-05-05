334b75b807b4257725b7fbdad8a5b8c31e2ab097
Carol <carol.tsai0224@gmail.com>
2022-05-05 14:01:34 -0400

    added user routes

--- models/Thought.js
+++ models/Thought.js
@@ -16,6 +16,10 @@ const thoughtSchema = new Schema({
    reactions: [reactionSchema],
 })
 
+thoughtSchema.virtual("reactionCount").get(function () {
+   return this.reactions.length;
+});
+
 const Thought = model("thought", thoughtSchema);
 
 module.exports = Thought;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,29 @@
+// user routes we need
+// get all users
+// get a single user by _id and populated friend and thought data
+// post a new user
+// put to update a user by its _id
+// removed a user by its _id with associated thoughts
+
+// routes for friends - /api/users/:userId/friends/:friendId
+// post to add a new friend to a user's friend list
+// delete to remove a friend from a user's friend list
+
+const router = require('express').Router();
+const {
+   getUsers,
+   getSingleUser,
+   createUser,
+   updateUser,
+   deleteUser,
+   addFriend,
+   removeFriend,
+} = require('../../controllers/studentController');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
